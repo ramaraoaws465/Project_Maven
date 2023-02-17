f08c25ed8aeaf1667f60e5697b3dbde7a6148349
ramarao465 <ramarao.465@gmail.com>
2023-02-17 02:12:01 +0000

    Java file modified

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMsg() {
     String someone = "World, Ramarao entered in Jenkins world";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
